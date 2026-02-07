fa29b4b6d06ba45213484d5880a45e92b4eb9e23
Holden Roaten <hroaten@Holdens-MacBook-Pro-170.local>
2026-02-07 16:11:33 -0500

    initialization

---
+++ bj_sim.py
@@ -0,0 +1,6 @@
+import math
+
+suits = ["Hearts", "Diamonds" , "Clubs", "Spades"]
+values = [ "2" , "3" , "4" , "5" , "6" , "7", "8", "9" , "10", "J" , "Q" , "K" , "A" ]
+deck = [(suit , value) for value in values for suit in suits]
+
